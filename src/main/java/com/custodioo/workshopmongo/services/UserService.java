322abc96b1f6301ac0e9e1d1f0af187983383ff7
Augusto Vicente <custodioovicente@outlook.com.br>
2022-03-27 18:33:23 -0300

    Getting an user by id

--- src/main/java/com/custodioo/workshopmongo/services/UserService.java
+++ src/main/java/com/custodioo/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.custodioo.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.custodioo.workshopmongo.domain.User;
 import com.custodioo.workshopmongo.repository.UserRepository;
+import com.custodioo.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
 	}
 }
